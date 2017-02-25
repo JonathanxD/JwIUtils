c41ad875dff26651c089db1830a39eae68e1b344
Jonathan <joniweb01@gmail.com>
2017-02-25 11:51:13 -0300

    More methods to CollectionUtils

--- src/main/java/com/github/jonathanxd/iutils/collection/CollectionUtils.java
+++ src/main/java/com/github/jonathanxd/iutils/collection/CollectionUtils.java
@@ -30,8 +30,15 @@ package com.github.jonathanxd.iutils.collection;
 import com.github.jonathanxd.iutils.exception.BiException;
 
 import java.lang.reflect.InvocationTargetException;
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.function.BiConsumer;
+import java.util.function.Supplier;
 
 /**
  * Created by jonathan on 05/03/16.
@@ -69,4 +76,86 @@ public class CollectionUtils {
 
     }
 
+    /**
+     * Create a {@link T Sequence} of {@link E}.
+     *
+     * @param elements Array Elements to add to sequence.
+     * @param factory  Factory method to create sequence.
+     * @param adder    Sequence adding method.
+     * @param <E>      Element type.
+     * @param <T>      Sequence type (non-java)
+     * @return Created {@link T Sequence} with {@code elements}.
+     */
+    public static <E, T> T sequenceOf(E[] elements, Supplier<T> factory, BiConsumer<T, E> adder) {
+        T sequence = factory.get();
+
+        for (E element : elements) {
+            adder.accept(sequence, element);
+        }
+
+        return sequence;
+    }
+
+    /**
+     * Create a {@link Collection} of {@link E} and add {@code elements} to the {@link Collection}.
+     *
+     * @param elements Elements to add to collection.
+     * @param factory  Collection factory method.
+     * @param <E>      Element type.
+     * @param <T>      Collection type.
+     * @return {@link T Collection} of {@link E} with {@code elements}.
+     */
+    public static <E, T extends Collection<E>> T collectionOf(E[] elements, Supplier<T> factory) {
+        T collection = factory.get();
+
+        Collections.addAll(collection, elements);
+
+        return collection;
+    }
+
+    /**
+     * Create a {@link List} of {@link E} and add {@code elements} to the {@link List}.
+     *
+     * @param elements Elements to add to list.
+     * @param <E>      Element type.
+     * @return {@link List} of {@link E} with {@code elements}.
+     */
+    public static <E> List<E> listOf(E[] elements) {
+        List<E> list = new ArrayList<>();
+
+        Collections.addAll(list, elements);
+
+        return list;
+    }
+
+    /**
+     * Create a {@link Set} of {@link E} and add {@code elements} to the {@link Set}.
+     *
+     * @param elements Elements to add to set.
+     * @param <E>      Element type.
+     * @return {@link Set} of {@link E} with {@code elements}.
+     */
+    public static <E> Set<E> setOf(E[] elements) {
+        Set<E> set = new HashSet<>();
+
+        Collections.addAll(set, elements);
+
+        return set;
+    }
+
+    /**
+     * Create a {@link LinkedHashSet} of {@link E} and add {@code elements} to the {@link
+     * LinkedHashSet}.
+     *
+     * @param elements Elements to add to set.
+     * @param <E>      Element type.
+     * @return {@link LinkedHashSet} of {@link E} with {@code elements}.
+     */
+    public static <E> LinkedHashSet<E> linkedSetOf(E[] elements) {
+        LinkedHashSet<E> set = new LinkedHashSet<>();
+
+        Collections.addAll(set, elements);
+
+        return set;
+    }
 }
