7a57843edab2458968c2378599cf87288e4e9b59
Jonathan <joniweb01@gmail.com>
2017-11-08 16:48:32 -0200

    Added more predicates, removed inner class

--- src/main/java/com/github/jonathanxd/iutils/function/Predicates.java
+++ src/main/java/com/github/jonathanxd/iutils/function/Predicates.java
@@ -27,6 +27,10 @@
  */
 package com.github.jonathanxd.iutils.function;
 
+import java.util.Objects;
+import java.util.function.DoublePredicate;
+import java.util.function.IntPredicate;
+import java.util.function.LongPredicate;
 import java.util.function.Predicate;
 
 /**
@@ -40,24 +44,136 @@ public final class Predicates {
 
 
     /**
-     * Returns a predicate that accepts all values.
+     * Returns a predicate that accepts any value.
      *
      * @param <T> Type.
-     * @return Predicate that accepts all values.
+     * @return Predicate that accepts any value.
      */
     @SuppressWarnings("unchecked")
     public static <T> Predicate<T> acceptAll() {
-        return (Predicate<T>) AcceptAllPredicate.INSTANCE;
+        return value -> true;
     }
 
-    static final class AcceptAllPredicate<T> implements Predicate<T> {
-
-        static final AcceptAllPredicate<?> INSTANCE = new AcceptAllPredicate<>();
-
-        @Override
-        public boolean test(T t) {
-            return true;
-        }
+    /**
+     * Returns a predicate that negates any value.
+     *
+     * @param <T> Type.
+     * @return Predicate that negates any value.
+     */
+    @SuppressWarnings("unchecked")
+    public static <T> Predicate<T> negateAll() {
+        return value -> false;
     }
 
+    /**
+     * Returns a predicate that accepts non-null values.
+     *
+     * @param <T> Type.
+     * @return Predicate that accepts non-null values.
+     */
+    @SuppressWarnings("unchecked")
+    public static <T> Predicate<T> notNull() {
+        return Objects::nonNull;
+    }
+
+    /**
+     * Returns a predicate that accepts null values.
+     *
+     * @param <T> Type.
+     * @return Predicate that accepts null values.
+     */
+    @SuppressWarnings("unchecked")
+    public static <T> Predicate<T> onlyNull() {
+        return Objects::isNull;
+    }
+
+    /**
+     * Returns a predicate that accepts positive ({@code > 0}) values.
+     *
+     * @return Predicate that accepts positive ({@code > 0}) values.
+     */
+    @SuppressWarnings("unchecked")
+    public static IntPredicate positive() {
+        return value -> value > 0;
+    }
+
+    /**
+     * Returns a predicate that accepts negative ({@code < 0}) values.
+     *
+     * @return Predicate that accepts negative ({@code < 0}) values.
+     */
+    @SuppressWarnings("unchecked")
+    public static IntPredicate negative() {
+        return value -> value < 0;
+    }
+
+    /**
+     * Returns a predicate that accepts only zero value.
+     *
+     * @return Predicate that accepts only zero value.
+     */
+    @SuppressWarnings("unchecked")
+    public static IntPredicate zero() {
+        return value -> value == 0;
+    }
+
+    /**
+     * Returns a predicate that accepts positive ({@code > 0}) values.
+     *
+     * @return Predicate that accepts positive ({@code > 0}) values.
+     */
+    @SuppressWarnings("unchecked")
+    public static LongPredicate positiveLong() {
+        return value -> value > 0;
+    }
+
+    /**
+     * Returns a predicate that accepts negative ({@code < 0}) values.
+     *
+     * @return Predicate that accepts negative ({@code < 0}) values.
+     */
+    @SuppressWarnings("unchecked")
+    public static LongPredicate negativeLong() {
+        return value -> value < 0;
+    }
+
+    /**
+     * Returns a predicate that accepts only zero value.
+     *
+     * @return Predicate that accepts only zero value.
+     */
+    @SuppressWarnings("unchecked")
+    public static LongPredicate zeroLong() {
+        return value -> value == 0L;
+    }
+
+    /**
+     * Returns a predicate that accepts positive ({@code > 0}) values.
+     *
+     * @return Predicate that accepts positive ({@code > 0}) values.
+     */
+    @SuppressWarnings("unchecked")
+    public static DoublePredicate positiveDouble() {
+        return value -> value > 0;
+    }
+
+    /**
+     * Returns a predicate that accepts negative ({@code < 0}) values.
+     *
+     * @return Predicate that accepts negative ({@code < 0}) values.
+     */
+    @SuppressWarnings("unchecked")
+    public static DoublePredicate negativeDouble() {
+        return value -> value < 0;
+    }
+
+    /**
+     * Returns a predicate that accepts only zero value.
+     *
+     * @return Predicate that accepts only zero value.
+     */
+    @SuppressWarnings("unchecked")
+    public static DoublePredicate zeroDouble() {
+        return value -> value == 0D;
+    }
 }
